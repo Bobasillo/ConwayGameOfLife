dd54626bab556ccd9c729b428c14e5c683dbc1a8
Bobas <trexdbg@gmail.com>
2026-02-04 20:23:46 +0100

    Primera version (No funcional)  Implementación de los step

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import numpy as np
 
-x = 20
-y = 30
+tamano_grid_x = 20
+tamano_grid_y = 30
 
-nx = len(range(x))
-ny = len(range(y))
+generacion = 0
 
-grid = np.zeros((nx, ny), dtype=bool)
-grid_en_buffer = np.zeros((nx, ny), dtype=bool)
+grid = np.random.choice([False, True], (tamano_grid_x, tamano_grid_y), p=[0.8, 0.2])
+grid_en_buffer = np.zeros((tamano_grid_x, tamano_grid_y), dtype=bool)
 
-# def update_grid(grid):
-#     for i in grid:
-#         for j in i:
-            
-    
 def reset_grid():
     grid[:, :] = grid_en_buffer[:, :]
-    grid_en_buffer[:, :] = np.zeros((nx, ny), dtype=bool)
-    
-update_grid(grid)
-reset_grid()
+    grid_en_buffer[:, :] = False
+
+def step(grid):
+    for x in range(tamano_grid_x):
+        for y in range(tamano_grid_y):
+            celulas_vivas = 0
+            for i in range(x-1, x+2):
+                for j in range(y-1, y+2):
+                    if (i, j) != (x, y):
+                        if grid[i % tamano_grid_x, j % tamano_grid_y]:
+                            celulas_vivas += 1
+            if grid[x, y]:
+                if celulas_vivas == 2 or celulas_vivas == 3:
+                    grid_en_buffer[x, y] = True
+            else:
+                if celulas_vivas == 3:
+                    grid_en_buffer[x, y] = True
+    reset_grid()
+    print(f"Gen {generacion}: {np.sum(grid)} vivas")
+    generacion += 1
+
+for i in range(100):
+    step(grid)
